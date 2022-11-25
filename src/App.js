e7b156ee551eeea0343336f3acaf9e667208e973
nidhiwisitech <nidhi@wisitech.com>
2022-11-25 16:43:28 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
     <Header/>
+    <Footer/>
+    
     <h1>We will start our First React project!! testing</h1>
     </>
   );
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return(
+       
+        <h1>This is Footer</h1>
+       
+    )
+    
+    };
+    
+    export default Header;
