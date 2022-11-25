fb4d85134a7fdcca339d544f5dc1a44f24215d89
harrywisitech <40620933+harrywisitech@users.noreply.github.com>
2022-11-25 16:08:48 +0530

    Header footer create component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
+    <>
+    <Header/>
     <h1>We will start our First React project!!</h1>
+    </>
   );
 }
 
